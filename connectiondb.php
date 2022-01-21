f5e28a68faa954ece8f523a80fa25c91a4fc85cc
Ege_Perim <perim.ege@gmail.com>
2022-01-21 16:35:09 +0300

    database change

--- connectiondb.php
+++ connectiondb.php
@@ -1,6 +1,6 @@
 <?php
 /*Author: Ege PERIM*/
 error_reporting(0);
-$connection_db=mysqli_connect("localhost","root","","tour_db");
+$connection_db=mysqli_connect("eu-cdbr-west-02.cleardb.net","b25764c3ea5462","615a7a38","heroku_de39cb12927bd1c");
 
 ?>
